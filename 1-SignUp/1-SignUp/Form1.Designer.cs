0b7906b63d7a0dcd1a52d446f12c9d1f7c9b90cf
xin chen <823515512@qq.com>
2020-10-30 15:53:01 +0800

    111

---
+++ 1-SignUp/1-SignUp/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace _1_SignUp
+{
+    partial class frm_SignUp
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_SignUp = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_No = new System.Windows.Forms.TextBox();
+            this.txt_Password = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_SignUp
+            // 
+            this.btn_SignUp.Location = new System.Drawing.Point(172, 185);
+            this.btn_SignUp.Name = "btn_SignUp";
+            this.btn_SignUp.Size = new System.Drawing.Size(75, 30);
+            this.btn_SignUp.TabIndex = 0;
+            this.btn_SignUp.Text = "注册";
+            this.btn_SignUp.UseVisualStyleBackColor = true;
+            this.btn_SignUp.Click += new System.EventHandler(this.btn_SignUp_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(41, 56);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 18);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "帐号：";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(41, 117);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(62, 18);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "密码：";
+            // 
+            // txt_No
+            // 
+            this.txt_No.Location = new System.Drawing.Point(126, 53);
+            this.txt_No.Name = "txt_No";
+            this.txt_No.Size = new System.Drawing.Size(218, 28);
+            this.txt_No.TabIndex = 3;
+            // 
+            // txt_Password
+            // 
+            this.txt_Password.Location = new System.Drawing.Point(126, 114);
+            this.txt_Password.Name = "txt_Password";
+            this.txt_Password.Size = new System.Drawing.Size(218, 28);
+            this.txt_Password.TabIndex = 4;
+            // 
+            // frm_SignUp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 260);
+            this.Controls.Add(this.txt_Password);
+            this.Controls.Add(this.txt_No);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btn_SignUp);
+            this.Name = "frm_SignUp";
+            this.Text = "注册";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_SignUp;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_No;
+        private System.Windows.Forms.TextBox txt_Password;
+    }
+}
+
---
+++ 1-SignUp/1-SignUp/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using SmartLinli.DatabaseDevelopement;
+
+namespace _1_SignUp
+{
+    public partial class frm_SignUp : Form
+    {
+        public frm_SignUp()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_SignUp_Click(object sender, EventArgs e)
+        {
+            string commandText =
+                $@"INSERT tb_Student (No,Password)
+                   VALUES('{this.txt_No.Text}','{this.txt_Password.Text}');";
+            SqlHelper sqlHelper = new SqlHelper();
+            int rowAffected = sqlHelper.QuickSubmit(commandText);
+
+            if (rowAffected==1)
+            {
+                MessageBox.Show("注册成功！");
+            }
+            else
+            {
+                MessageBox.Show("该用户已存在！注册失败！");
+            }
+        }
+        
+    }
+}
